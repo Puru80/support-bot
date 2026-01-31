f0fbc412f44440889c27b6e128a5ea9a04bbfce8
Puru80 <puru.agar99@gmail.com>
2026-01-31 19:48:43 +0530

    RAG Support bot for JAVA Interview Prep

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
 
-# --- 1. SETUP & CONFIG ---
+load_dotenv() # Load environment variables
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+if google_api_key:
+    print(f"GOOGLE_API_KEY successfully loaded.")
+else:
+    print("GOOGLE_API_KEY not found in environment variables or .env file.")
+
 app = FastAPI(title="Java Support Bot API")
+
+# Initialize LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=google_api_key, temperature=0)
 
 # Initialize Embeddings (Must match Phase 3)
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
@@ -19,15 +31,13 @@ vector_db = Chroma(
     embedding_function=embeddings
 )
 
-# Initialize LLM
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-
 # --- 2. RAG LOGIC ---
 # the system prompt (The "Guardrails")
 system_prompt = (
     "You are an expert QnA assistant. Use the following pieces of retrieved "
     "context to answer the user's question. If the answer isn't in the context, "
     "say you don't know—don't try to make up an answer. Keep it concise.\n\n"
+    "{context}"
 )
 
 prompt = ChatPromptTemplate.from_messages([
@@ -38,7 +48,6 @@ prompt = ChatPromptTemplate.from_messages([
 # Create the chains
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(vector_db.as_retriever(), question_answer_chain)
-
 
 # --- 3. API ENDPOINTS ---
 class QueryRequest(BaseModel):
@@ -60,4 +69,4 @@ async def ask_bot(request: QueryRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
